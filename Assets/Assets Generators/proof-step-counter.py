de59d64652ef2d35da7691c10eab7be503803bed
jableable <jable@iu.edu>
2024-07-26 13:38:30 -0400

    Fix counting error in proof-step-counter.py
    Added \s\s\s to regex

--- Assets/Assets Generators/proof-step-counter.py
+++ Assets/Assets Generators/proof-step-counter.py
@@ -6,20 +6,19 @@
 
 import re
 
-# open file
+# open files
 with open('./efgcpbllemb-mm.txt', 'r') as mm, open('./efgcpbllemb-mm-pa.txt', 'r') as mmpa:
-        mm_counter = 0
-        mmpa_counter = 0
+        mm_counter = 0  # counts proof steps from MM file
+        mmpa_counter = 0    # counts proof steps from MM-PA file
         for line in mm:
-            mm_line_num = re.search("(^((|\s|\s\s)[0-9]+))(=?(\s+.+?)=)",line)
+            mm_line_num = re.search("(^((|\s|\s\s|\s\s\s)[0-9]+))(=?(\s+.+?)=)",line)
             if mm_line_num is not None:
-                print(mm_line_num)
                 mm_counter += 1
                 final_mm_line_num = mm_line_num
         for line in mmpa:
-            mmpa_line_num = re.search("^((|\s|\s\s)[0-9]+)(=?(\s+.+?=))",line)
+            mmpa_line_num = re.search("(^((|\s|\s\s|\s\s\s)[0-9]+))(=?(\s+.+?)=)",line)
             if mmpa_line_num is not None:
                 mmpa_counter += 1
                 final_mmpa_line_num = mmpa_line_num
         print(f"The final numbered step from MM is {final_mm_line_num.group(1)} and the total number of steps in MM is {mm_counter}")
-        print(f"The final numbered step from MM is {final_mmpa_line_num.group(1)} and the total number of steps in MM is {mmpa_counter}")
+        print(f"The final numbered step from MM-PA is {final_mmpa_line_num.group(1)} and the total number of steps in MM-PA is {mmpa_counter}")
