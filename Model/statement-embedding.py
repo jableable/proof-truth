2e584d70b38cc8f3bb40b3bfe033ce9b4a045619
jableable <jable@iu.edu>
2024-08-08 13:28:55 -0400

    Edit statement-embedding.py to be callable func
    This file now contains the function create_emb
    which can be called by other files to create the
    vector embedding of a given feature pair of
    thm label and thm statemnt

--- Model/statement-embedding.py
+++ Model/statement-embedding.py
@@ -1,4 +1,5 @@
-# given a logical statement like the following from 36622 lcfrvalsnN:
+# given a logical statement like the following proof step from lcfrvalsnN 
+# (38836 in MM, 36622 in tag_proof.csv, 36284 in proof_graph.pkl):
 # 400: '$p |- ( ( G e. R /\ x e. ( ._|_ ` ( L ` G ) ) ) -> E. f e. R x e. ( ._|_ ` ( L ` f ) ) )'
 # use vocab.txt to find its embedding vector
 
@@ -9,20 +10,41 @@
 
 import numpy as np
 
-vector_size = 500   # 500 is placeholder for longest logical statement from tag_proof.csv
-embedding = np.zeros(vector_size, dtype=int)   # initialize embedding vector
-stmt = '( ( G e. R /\ x e. ( ._|_ ` ( L ` G ) ) ) -> E. f e. R x e. ( ._|_ ` ( L ` f ) ) )'
 
-split_stmt = stmt.split()   # remove spaces and place each character into ordered list
-
-with open("./Assets/vocab.txt", "r") as input:
-    voc_index = {}
-    for num, line in enumerate(input):  # correspond vocabulary with numbers in range 1-1598
-        line = line.split() # get rid of \n
-        voc_index[line[0]] = num+1  # shift by one so that 0 corresponds to space
-    for num, char in enumerate(split_stmt):
-        embedding[num] = voc_index[char]
-
-print(embedding)
+# create portion of embedding from theorem label; returns num of label from labels.txt
+def get_thm_label_num(lbl):
+    with open("../Assets/labels.txt", "r") as lbl_nums:
+        for num, line in enumerate(lbl_nums):
+            line = line.split() # get rid of \n
+            if lbl in line:
+                return num+1    #shift by 1 to match lines nums from labels.txt
+        print("didn't find it...")
+        return
 
 
+# create portion of embedding from theorem statement; returns embedding vector
+def get_thm_stmt_emb(stmt):
+    vector_size = 500   # 500 is placeholder for longest logical statement from tag_proof.csv
+    emb = np.zeros(vector_size, dtype=int)   # initialize embedding vector
+
+    split_stmt = stmt.split()   # remove spaces from stmt and place each character into ordered list
+
+    with open("../Assets/vocab.txt", "r") as input:
+        voc_index = {}
+        for num, line in enumerate(input):  # correspond vocabulary with numbers in range 1-1598 within voc_index dict
+            line = line.split() # get rid of \n
+            voc_index[line[0]] = num+1  # shift by 1 so that 0 corresponds to space
+        for num, char in enumerate(split_stmt): # place index for char from voc_index into embedding
+            emb[num] = voc_index[char]
+    return emb
+
+if __name__ == "__main__":
+    #temporary example input of stmt and lbl
+    #lbl = 'rspcev'  
+    #stmt = '( ( G e. R /\ x e. ( ._|_ ` ( L ` G ) ) ) -> E. f e. R x e. ( ._|_ ` ( L ` f ) ) )'
+  
+
+    def create_emb(lbl,stmt):
+        #prepend emb from get_thm_stmt_emb(stmt) with num from get_thm_label_num(lbl)
+        total_emb = np.insert(get_thm_stmt_emb(stmt),0,get_thm_label_num(lbl))
+        return total_emb
