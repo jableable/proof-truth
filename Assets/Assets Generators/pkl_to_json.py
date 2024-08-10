68653e6e4769d5c01b1d97fbd0a104e60949a332
jableable <jable@iu.edu>
2024-08-10 19:26:48 -0400

    Create pkl_to_json.py
    Used to convert .pkl file to .json dict
    
    .json dict is then used to make Dataset

---
+++ Assets/Assets Generators/pkl_to_json.py
@@ -0,0 +1,24 @@
+import pickle
+import json
+import numpy as np
+
+# open the .pkl file
+with open('../proof_graph.pkl', 'rb') as file:
+    raw_data = pickle.load(file)
+
+#the following class should allow for encoding of np objects in .json format
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super().default(obj)
+    
+# write the .json file; 
+# this .json file is used in creation of Dataset class
+# Dataset class makes its own .json file to replace statements with vector embeddings
+with open('../unprocessed_data.json', 'w') as f:
+    json.dump(raw_data, f, cls=NumpyEncoder)
+
+# open the .json file; could also use pd.read_json
+# with open("./data/data.json") as file:
+    # json_load = json.load(file)
