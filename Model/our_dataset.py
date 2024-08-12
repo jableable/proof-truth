b6376a18fb9332c170355cf9a22ae70cb5c92da6
jableable <jable@iu.edu>
2024-08-12 08:33:48 -0400

    Add file_limit support to ProofDataset class

--- Model/our_dataset.py
+++ Model/our_dataset.py
@@ -7,36 +7,49 @@ from torch_geometric.data import Data, Dataset
 
 
 
-# Create ProofDataset class, which is called by modelA.py
-# When calling ProofDataset(root="data/") in modelA.py, all processed .pt files are generated
+# Create ProofDataset class, which is called by modelA.ipynb
+# When calling ProofDataset(root="data/",file_limit = 100) in modelA.py, first 100 processed .pt files are generated
 # See https://pytorch-geometric.readthedocs.io/en/latest/tutorial/create_dataset.html
 # for more info
 
 class ProofDataset(Dataset):
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None, file_limit=None):
+        self.file_limit = file_limit    # make file limit able to be called later
         super().__init__(root, transform, pre_transform, pre_filter)
 
+    # indicate data file in data/raw to be processed
     @property
     def raw_file_names(self):
         return "data.json"
 
+    # determine files to be checked when assembling dataset
     @property
-    def processed_file_names(self):
-        self.data = pd.read_json(self.raw_paths[0])
-        return ['data_'+str(idx)+'.pt' for idx, _ in enumerate(self.data)]
+    def processed_file_names(self):        
+        self.data = pd.read_json(self.raw_paths[0]) # read in .json file
+        # in next two if statements, either implement file_limit, or don't
+        if self.file_limit is None:
+            index_set = len(self.data.columns)
+        if self.file_limit is not None:
+            index_set = self.file_limit
+        return ['data_'+str(idx)+'.pt' for idx in range(index_set)]
 
+    # download from url not currently supported
     def download(self):
-        # Download to `self.raw_dir`.
-        #path = download_url(url, self.raw_dir)
         pass
 
+    # create missing .pt files according to file_limit
     def process(self):
         self.data = pd.read_json(self.raw_paths[0])
-        for index, _ in enumerate(self.data):
+        self.data = self.data.drop(["edge_attr"])
+        if self.file_limit is None:
+            index_set = len(self.data.columns)
+        if self.file_limit is not None:
+            index_set = self.file_limit
+        for index in range(index_set):   # loop through graphs specified by file_limit
 
-            x = torch.Tensor([])
+            x = torch.Tensor([])    # initialize feature tensor for entire graph
 
-            for i, step in enumerate(self.data[index]["x"]):
+            for i, _ in enumerate(self.data[index]["x"]):   # loop through each proof step
                 lbl = self.data[index]["x"][i][1]["label"]
                 stmt = self.data[index]["x"][i][1]["statement"]
                 emb = torch.Tensor([create_emb(lbl,stmt)])
@@ -69,3 +82,8 @@ class ProofDataset(Dataset):
     def get(self, idx):
         data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
         return data
+    
+
+if __name__ == "__main__":
+    test_obj = ProofDataset(root="data/",file_limit=100)
+    print(test_obj)
